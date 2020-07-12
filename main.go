4b44cd874c9b10a2e77d76fa63e8fe7fe970cd51
Martín Montes <martin11lrx@gmail.com>
2020-07-12 23:08:29 +0200

    Refactored into smalled funcs

--- main.go
+++ main.go
@@ -19,11 +19,11 @@ var (
 	serverAddr = flag.String("server_addr", "localhost:11000", "The server address in the format of host:port")
 )
 
-func proxyGRPC(backendAddr string, r *http.Request) (*httputil.ReverseProxy, error) {
+func proxyGRPC(serverAddr string, r *http.Request) *httputil.ReverseProxy {
 	proxy := &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			r.URL.Scheme = "https"
-			r.URL.Host = backendAddr
+			r.URL.Host = serverAddr
 		},
 		Transport: &http2.Transport{
 			DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
@@ -31,7 +31,7 @@ func proxyGRPC(backendAddr string, r *http.Request) (*httputil.ReverseProxy, err
 			},
 		},
 		ModifyResponse: func(res *http.Response) error {
-			log.Printf("%s %s => %s %d", r.Method, r.URL, backendAddr, res.StatusCode)
+			log.Printf("%s %s => %s %d", r.Method, r.URL, serverAddr, res.StatusCode)
 			return nil
 		},
 		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
@@ -39,25 +39,26 @@ func proxyGRPC(backendAddr string, r *http.Request) (*httputil.ReverseProxy, err
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		},
 	}
-	return proxy, nil
+	return proxy
+}
+
+func router(serverAddr string) *mux.Router {
+	router := mux.NewRouter()
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p := proxyGRPC(serverAddr, r)
+		p.ServeHTTP(w, r)
+	})
+	return router
 }
 
 func main() {
 	flag.Parse()
-
-	router := mux.NewRouter()
-	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		p, err := proxyGRPC(*serverAddr, r)
-		if err != nil {
-			log.Print(err)
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
-		}
-		p.ServeHTTP(w, r)
-	})
-
-	log.Printf("Proxy listening on port %d", *port)
+	router := router(*serverAddr)
 	addr := fmt.Sprintf(":%d", *port)
-	err := http.ListenAndServeTLS(addr, testdata.Path("server1.pem"), testdata.Path("server1.key"), router)
+	cert := testdata.Path("server1.pem")
+	key := testdata.Path("server1.key")
+	log.Printf("Proxy listening on port %d", *port)
+	err := http.ListenAndServeTLS(addr, cert, key, router)
 	if err != nil {
 		log.Fatal(err)
 	}
